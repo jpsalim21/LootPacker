093b6d9fd8b9e4a616d776deeb284227f6204f24
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-02-12 00:21:06 -0300

    Modifica as animações do jogador

--- Player.gd
+++ Player.gd
@@ -1,16 +1,11 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+const SPEED = 100.0
 
 func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	var direction2 = Input.get_axis("ui_up", "ui_down")
+	
 	var directionVector = Vector2(direction, direction2)
 	velocity = directionVector.normalized() * SPEED
 	move_and_slide()
